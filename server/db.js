6352f6c87555873b108011a24586efe7bfe8b0f0
Biruk Zewdu <Brookzewdu09@gmail.com>
2023-06-17 14:41:46 +0300

    folder restructure

--- server/db.js
+++ server/db.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+
+
 const connection = mysql.createPool({
     host: "127.0.0.1",
     user: "root",
@@ -8,7 +10,8 @@ const connection = mysql.createPool({
     database: "Airport_Management_system", 
     port: 3306,
 });
-// console.log(process.env.MYSQL_PASSWORD);
+
+
 connection.getConnection(function(err) {
     if (err) {
         console.error("Connection err: " + err.stack);
